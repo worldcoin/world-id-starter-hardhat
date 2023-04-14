7dd7e6d5da0dceeb93a0286932bf713522912140
Miguel Piedrafita <github@miguel.build>
2023-04-15 00:33:35 +0900

    lint

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -29,7 +29,11 @@ contract Contract {
     /// @param _worldId The WorldID instance that will verify the proofs
     /// @param _appId The World ID app ID
     /// @param _actionId The World ID action ID
-    constructor(IWorldID _worldId, string memory _appId, string memory _actionId) {
+    constructor(
+        IWorldID _worldId,
+        string memory _appId,
+        string memory _actionId
+    ) {
         worldId = _worldId;
         externalNullifier = abi
             .encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
